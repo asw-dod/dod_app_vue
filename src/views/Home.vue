2a9af0defed1db1606396313adcf2de23e59276e
INMD1 <lyw514549@gmail.com>
2022-03-20 22:57:41 +0900

    1차 디자인중 현재 진행 상황 업데이트

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <ion-page>
+    <ion-row> </ion-row>
+    <ion-row class="row1">
+      <ion-col>
+        <h1>학사공지</h1>
+        <!--listview-->
+        <!--for 문으로 리스트 데이터를 가지고 온다.-->
+        <ion-card class="table">
+          <ion-card-header>
+            <ion-card-title>Card Title</ion-card-title>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+          </ion-card-header>
+        </ion-card>
+      </ion-col>
+    </ion-row>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { IonPage, IonRow, IonCol } from "@ionic/vue";
+
+export default defineComponent({
+  name: "Tab1Page",
+  components: { IonPage, IonRow, IonCol },
+});
+</script>
+
+<style>
+.topimage {
+  height: 30vh;
+  width: 100%;
+  object-fit: cover;
+}
+.row1 {
+  height: 80vh;
+  background-color: aliceblue;
+}
+
+.table {
+  height: 85%;
+}
+</style>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,37 +4,42 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="home" />
+          <ion-label>Home</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="albums" />
+          <ion-label>비교과</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="clipboard" />
+          <ion-label>취업</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="">
+          <ion-icon :icon="apps" />
+          <ion-label>동의대 2.0</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { home, albums, clipboard, apps } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      home,
+      albums, 
+      clipboard, 
+      apps,
     }
   }
 });
