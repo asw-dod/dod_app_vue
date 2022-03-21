02485947f6dfa54db69e30162d3874429bd52d6f
INMD1 <87979171+INMD1@users.noreply.github.com>
2022-03-22 00:18:57 +0900

    디자인 제작중 체크 포인트

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,105 @@
 <template>
   <ion-page>
-    <ion-row> </ion-row>
-    <ion-row class="row1">
-      <ion-col>
-        <h1>학사공지</h1>
-        <!--listview-->
-        <!--for 문으로 리스트 데이터를 가지고 온다.-->
-        <ion-card class="table">
-          <ion-card-header>
-            <ion-card-title>Card Title</ion-card-title>
-            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-          </ion-card-header>
-        </ion-card>
-      </ion-col>
-    </ion-row>
+    <!--header-->
+    <ion-toolbar>
+      <ion-title>D.O.D Dap notice</ion-title>
+    </ion-toolbar>
+    <!--body-->
+    <ion-content fullscreen>
+      <ion-row class="row1">
+        <ion-grid>
+          <!--학사공지-->
+          <ion-col>
+            <h1 class="note_title">학사공지</h1>
+            <!--for 문으로 리스트 데이터를 가지고 온다.-->
+            <ion-card class="table">
+              <ion-card-header>
+                <ion-card-title>Card Title</ion-card-title>
+                <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <!--장학공지-->
+            <h1 class="note_title">장학공지</h1>
+            <!--listview-->
+            <!--for 문으로 리스트 데이터를 가지고 온다.-->
+            <ion-card class="table">
+              <ion-card-header>
+                <ion-card-title>Card Title</ion-card-title>
+                <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col>
+            <!--장학공지-->
+            <h1 class="note_title">기숙사공지</h1>
+            <!--listview-->
+            <!--for 문으로 리스트 데이터를 가지고 온다.-->
+            <ion-card class="table">
+              <ion-card-header>
+                <ion-card-title>Card Title</ion-card-title>
+                <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-grid>
+      </ion-row>
+      <!--버튼-->
+      <ion-fab horizontal="end" vertical="bottom" slot="fixed">
+        <ion-fab-button color="dark">
+          <ion-icon :icon="settings"></ion-icon>
+        </ion-fab-button>
+        <ion-fab-list side="top">
+          <ion-fab-button color="light">
+            <ion-icon :icon="contrast"></ion-icon>
+          </ion-fab-button>
+        </ion-fab-list>
+      </ion-fab>
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { IonPage, IonRow, IonCol } from "@ionic/vue";
-
+import {
+  IonPage,
+  IonRow,
+  IonCol,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonIcon,
+  IonFab,
+  IonFabButton,
+  IonFabList,
+} from "@ionic/vue";
+import { cog, settings, contrast } from "ionicons/icons";
 export default defineComponent({
   name: "Tab1Page",
-  components: { IonPage, IonRow, IonCol },
+  components: {
+    IonPage,
+    IonRow,
+    IonCol,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonIcon,
+    IonFab,
+    IonFabButton,
+    IonFabList,
+  },
+  setup() {
+    return { cog, settings, contrast };
+  },
+  methods: {},
 });
 </script>
-
-<style>
-.topimage {
-  height: 30vh;
-  width: 100%;
-  object-fit: cover;
-}
-.row1 {
-  height: 80vh;
-  background-color: aliceblue;
-}
-
-.table {
-  height: 85%;
-}
-</style>
